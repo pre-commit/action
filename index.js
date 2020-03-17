30b00a6cd975a75c7a08e96f1de0d06a3bdb897b
Deepyaman Datta <deepyaman.datta@utexas.edu>
2020-03-17 18:30:51 -0500

    Support `extra_args` keyword in pre-commit GitHub action

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
-
-const ARGS = [
-    'run', '--all-files', '--show-diff-on-failure', '--color=always'
-];
+const tr = require('@actions/exec/lib/toolrunner');
 
 function addToken(url, token) {
-    return url.replace(/^https:\/\//, `https://x-access-token:${token}@`)
+    return url.replace(/^https:\/\//, `https://x-access-token:${token}@`);
 }
 
 async function main() {
@@ -16,15 +13,21 @@ async function main() {
         await exec.exec('pip', ['freeze', '--local']);
     });
 
+    const args = [
+        'run',
+        '--show-diff-on-failure',
+        '--color=always',
+        ...tr.argStringToArray(core.getInput('extra_args')),
+    ];
     const token = core.getInput('token');
     const pr = github.context.payload.pull_request;
     const push = !!token && !!pr;
-    const ret = await exec.exec('pre-commit', ARGS, {ignoreReturnCode: push});
+    const ret = await exec.exec('pre-commit', args, {ignoreReturnCode: push});
     if (ret && push) {
         // actions do not run on pushes made by actions.
         // need to make absolute sure things are good before pushing
         // TODO: is there a better way around this limitation?
-        await exec.exec('pre-commit', ARGS);
+        await exec.exec('pre-commit', args);
 
         const diff = await exec.exec(
             'git', ['diff', '--quiet'], {ignoreReturnCode: true}
