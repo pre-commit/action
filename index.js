4a08748ace346be6b06cec8872f8f47006efc238
Trim21 <i@trim21.me>
2020-06-08 22:56:46 +0800

    embedding cache steps by using @actions/cache

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
 const tr = require('@actions/exec/lib/toolrunner');
+const cache = require('@actions/cache');
+
+const paths = [path.join(os.homedir(), '.cache', 'pre-commit')];
+
+async function getPythonVersion() {
+    let myOutput = '';
+    const options = {
+        silent: true,
+        listeners: {
+            stdout: (data) => {
+                myOutput += data.toString();
+            }
+        }
+    };
+
+    await exec.exec('python', ['-VV'], options);
+    await exec.exec('which', ['python'], options);
+    return myOutput;
+}
+
+function hashFile(filePath) {
+    return hashString(fs.readFileSync(filePath).toString());
+
+}
+
+function hashString(content) {
+    const sha256 = crypto.createHash('sha256');
+    return sha256.update(content).digest('hex');
+}
+
+async function setupCache(cacheKey) {
+    try {
+        await cache.saveCache(paths, cacheKey);
+    } catch (e) {
+        if (e.toString().includes('reserveCache failed')) {
+            // race condition
+            core.info(e.message);
+            return;
+        }
+        throw e;
+    }
+}
+
+async function restoreCache(cacheKey) {
+    return !!(await cache.restoreCache(paths, cacheKey));
+}
 
 function addToken(url, token) {
     return url.replace(/^https:\/\//, `https://x-access-token:${token}@`);
@@ -22,7 +73,13 @@ async function main() {
     const token = core.getInput('token');
     const pr = github.context.payload.pull_request;
     const push = !!token && !!pr;
+
+    const pyVer = await getPythonVersion();
+    const cacheKey = `pre-commit-1-${hashString(pyVer)}-${hashFile('.pre-commit-config.yaml')}`;
+    await restoreCache(cacheKey);
     const ret = await exec.exec('pre-commit', args, {ignoreReturnCode: push});
+    await setupCache(cacheKey);
+
     if (ret && push) {
         // actions do not run on pushes made by actions.
         // need to make absolute sure things are good before pushing
